48855c9776533efab8db8dcf525d70b60ee818d7
Alona Tiunina <163348743+tiunina@users.noreply.github.com>
2024-11-10 11:47:09 -0800

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import Contact from "../Contact/Contact";
+import { selectFilteredContacts } from "../../redux/contacts/contactsSlice";
+import { deleteContact } from "../../redux/contactsOps";
+// import { selectFilter } from "../../redux/filter/filtersSlice";
+// import { deleteContact, selectItems } from "../../redux/contacts/contactsSlice";
+
+const ContactList = () => {
+  const dispatch = useDispatch();
+  const filteredContacts = useSelector(selectFilteredContacts);
+  // const items = useSelector(selectItems);
+
+  // const filter = useSelector(selectFilter);
+
+  // const filteredContacts = items.filter((contact) =>
+  //   contact.name.toLowerCase().includes(filter.toLowerCase())
+  // );
+
+  const onDelete = (itemId) => {
+    // const action = deleteContact(itemId);
+    // dispatch(action);
+    dispatch(deleteContact(itemId));
+  };
+  return (
+    <ul>
+      {filteredContacts.map((contactItem) => {
+        return (
+          <li key={contactItem.id}>
+            <Contact contactItem={contactItem} onDelete={onDelete} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
